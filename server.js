b211d2803ecaefb84bc3af62ae8feeb2b36ecf11
Emmanuel <oladipoemmanuel453@gmail.com>
2025-06-09 19:53:18 +0100

    BCC Gallery

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(cors({
       ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control']
 }));
 
 const port = 6500;
