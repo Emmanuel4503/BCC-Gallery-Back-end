0e7819c14afbc0b1a943008b2ef0f111ae8be380
Emmanuel <oladipoemmanuel453@gmail.com>
2025-07-07 22:11:35 +0100

    BCC Gallery

--- server.js
+++ server.js
@@ -14,8 +14,6 @@ app.use(cors({
     origin: [
         'https://bccgallery.netlify.app',
         'https://bccgalleryadmin.netlify.app',
-        'https://bcc-gallery-back-end.onrender.com',
-        'https://bcc-gallery-back-end-production.up.railway.app'
       ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
